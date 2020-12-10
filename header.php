e96f06cbcc2ec4e8d9ee3464a8dbea1de31db848
SANI <sanii.trinh@gmail.com>
2020-12-10 12:29:25 -0800

    add basic structure files

---
+++ header.php
@@ -0,0 +1,11 @@
+<html>
+
+<head>
+<link rel="stylesheet" href="<?php echo site_url(); ?>/wp-content/themes/charleschang/style.css">
+    <?php wp_head(); ?>
+ </head>
+
+<body>
+    <header>
+        Header
+    </header>
